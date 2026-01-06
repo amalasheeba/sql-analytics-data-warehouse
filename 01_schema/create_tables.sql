17f50075c86805cdc248d7ea9d872817f41c2458
amalasheeba <64658484+amalasheeba@users.noreply.github.com>
2026-01-06 17:35:18 +0530

    Update create_tables.sql

--- 01_schema/create_tables.sql
+++ 01_schema/create_tables.sql
@@ -8,3 +8,45 @@ CREATE TABLE dim_patient (
     effective_end     DATE,
     is_current        CHAR(1)
 );
+
+CREATE TABLE dim_doctor (
+    doctor_pk    INT IDENTITY PRIMARY KEY,
+    doctor_id    VARCHAR(50),
+    doctor_name  VARCHAR(100),
+    specialization VARCHAR(100)
+);
+
+CREATE TABLE dim_hospital (
+    hospital_pk  INT IDENTITY PRIMARY KEY,
+    hospital_id  VARCHAR(50),
+    hospital_name VARCHAR(100),
+    city         VARCHAR(50),
+    state        VARCHAR(50)
+);
+
+CREATE TABLE dim_date (
+    date_pk       INT PRIMARY KEY,
+    full_date     DATE,
+    year          INT,
+    quarter       INT,
+    month         INT,
+    day           INT,
+    weekday       VARCHAR(10)
+);
+
+CREATE TABLE fact_patient_visit (
+    visit_id      VARCHAR(50),
+    patient_pk    INT,
+    doctor_pk     INT,
+    hospital_pk   INT,
+    date_pk       INT,
+    diagnosis     VARCHAR(200),
+    treatment_cost DECIMAL(10,2),
+
+    CONSTRAINT fk_patient FOREIGN KEY (patient_pk) REFERENCES dim_patient(patient_pk),
+    CONSTRAINT fk_doctor FOREIGN KEY (doctor_pk) REFERENCES dim_doctor(doctor_pk),
+    CONSTRAINT fk_hospital FOREIGN KEY (hospital_pk) REFERENCES dim_hospital(hospital_pk),
+    CONSTRAINT fk_date FOREIGN KEY (date_pk) REFERENCES dim_date(date_pk)
+);
+
+
